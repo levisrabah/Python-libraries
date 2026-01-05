3971bc7aca8f0aa9741c86b25ff9164a791bb851
levisrabah <levisrabah@gmail.com>
2026-01-05 16:27:37 +0300

    python libraries

---
+++ index.py
@@ -0,0 +1,6 @@
+#import math
+#print(math.pi)
+#print(math.sqrt(100))
+import random
+print("Random number between 1 and 1000:", random.randint(1, 1000))
+print("Random person from list:",random.choice(['Maurine','Stacy','Levis','Vall','Mitchel','Samson', 'Moses', 'Whitney']))
